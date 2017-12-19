4a648956ed01b4b7862cde5f7c3cc71ab2067a47
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-12-19 11:46:25 -0500

    metaclass for data classes

---
+++ gaia/dr1/core.py
@@ -0,0 +1,62 @@
+# Standard library
+import abc
+from collections import namedtuple
+
+__all__ = ['ComponentMapping', 'DataMeta']
+
+
+_ComponentMappingBase = \
+    namedtuple('ComponentMapping',
+               ('obj_name', 'tbl_name', 'tbl_unit'))
+
+
+class ComponentMapping(_ComponentMappingBase):
+    """
+    This `~collections.namedtuple` is used to map from raw column names to
+    common attribute names. For example, from ``pmra`` to ``pm_ra_cosdec``.
+    """
+
+    def __new__(cls, obj_name, tbl_name, tbl_unit):
+        # this trick just provides some defaults
+        return super().__new__(cls, obj_name, tbl_name, tbl_unit)
+
+
+class DataMeta(abc.ABCMeta):
+    def __new__(mcls, name, bases, members):
+
+        if 'component_mappings' in members:
+            mappings = members['component_mappings']
+            found_mappings = True
+
+        else:
+            found_mappings = False
+
+        # somewhat hacky, but this is the best way to get the MRO according to
+        # https://mail.python.org/pipermail/python-list/2002-December/167861.html
+        tmp_cls = super().__new__(mcls, name, bases, members)
+
+        for m in (c.__dict__ for c in tmp_cls.__mro__):
+            if (not found_mappings and 'component_mappings' in m):
+                mappings = m['component_mappings']
+                found_mappings = True
+
+            if found_mappings:
+                break
+        else:
+            raise ValueError("Couldn't find component_mappings frame "
+                             "attribute.")
+
+        members['_tbl_to_obj'] = dict([(m.tbl_name, m.obj_name)
+                                       for m in mappings])
+        members['_obj_to_tbl'] = dict([(m.obj_name, m.tbl_name)
+                                       for m in mappings])
+        members['_tbl_to_unit'] = dict([(m.tbl_name, m.tbl_unit)
+                                        for m in mappings])
+        members['_obj_to_unit'] = dict([(m.obj_name, m.tbl_unit)
+                                        for m in mappings])
+
+        # Cache a list of extra names to add to dir() so autocomplete works on
+        # the object attribute names instead of the table column names
+        members['_extra_dir_names'] = list(members['_obj_to_tbl'].keys())
+
+        return super().__new__(mcls, name, bases, members)
