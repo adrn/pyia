aa2e3f917cd273e7e8914ac7a1c80795b260e9ff
Adrian Price-Whelan <adrian.prw@gmail.com>
2017-12-19 09:22:49 -0500

    initial commit with package template

---
+++ gaia/__init__.py
@@ -0,0 +1,9 @@
+# Packages may add whatever they like to this file, but
+# should keep this content at the top.
+# ----------------------------------------------------------------------------
+from ._astropy_init import *
+# ----------------------------------------------------------------------------
+
+if not _ASTROPY_SETUP_:
+    # For egg_info test builds to pass, put package imports here.
+    pass
