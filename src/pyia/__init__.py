ec89c3c931823ba1b1873eea15be83ef22937b32
Adrian Price-Whelan <adrian.prw@gmail.com>
2023-12-05 21:55:34 -0500

    overhaul package infrastructure to use scientific-python's cookiecutter package template

---
+++ src/pyia/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2023 Adrian Price-Whelan. All rights reserved.
+
+pyia: A Python package for working with data from the Gaia mission
+"""
+
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
